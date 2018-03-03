bceb181fac4d8e37f383ffcbd90c0da86a7a60f1
AlexandreBarde <19241664+AlexandreBarde@users.noreply.github.com>
2018-03-03 15:20:28 +0100

    fin exo2 ajax

--- Ajax/Exo2/script.js
+++ Ajax/Exo2/script.js
@@ -1,15 +1,79 @@
-document.getElementById('cp').addEventListener("onblur", checkCodePostal);
+document.getElementById('cp').addEventListener("blur", checkCodePostal);
+document.getElementById('erreur').style.visibility = "hidden";
 
 function checkCodePostal()
 {
+    var codePostal = document.getElementById('cp').value;
     var xhr = getXhr();
     xhr.onreadystatechange = function()
     {
         if(xhr.readyState == 4 && xhr.status == 200)
         {
-            listeVilles = xhr.responseText.split("\n");
+            if(!is_numeric(document.getElementById('cp').value))
+            {
+                console.log("erreur");
+                document.getElementById('erreur').style.visibility = "visible";
+                document.getElementById('cp').style.borderStyle = 'solid';
+                document.getElementById('cp').className = 'test';
+            }
+            else
+            {
+                document.getElementById('cp').className = '';
+                document.getElementById('erreur').style.visibility = "hidden";
+                document.getElementById('cp').style.borderStyle = '';
+                document.getElementById('cp').style.borderColor = '';
+                var json = JSON.parse(xhr.responseText);
+                var nombreVilles = json.length;
+                if(nombreVilles > 1)
+                {
+                    if(document.getElementById('villeDIV').contains(document.getElementById('ville')))
+                    {
+                        document.getElementById('ville').remove();
+                        console.log("delete ville");
+                    }
+                    if(document.getElementById('villeDIV').contains(document.getElementById('villes')))
+                    {
+                        var villesElement = document.getElementById('villes');
+                        villesElement.options.length = 0;
+                    }
+                    else
+                    {
+                        var element = document.createElement('select');
+                        element.id = "villes";
+                        document.getElementById('villeDIV').appendChild(element);
+                        document.getElementById('villes').options.length = 0;
+                    }
+                    listeVilles = document.getElementById('villes');
+                    for (var i = 0; i < nombreVilles; i++)
+                    {
+                        var opt = document.createElement('option');
+                        opt.value = i;
+                        opt.innerHTML = json[i].nomVille;
+                        listeVilles.appendChild(opt);
+                    }
+                }
+                else
+                {
+                    if(document.getElementById('villeDIV').contains(document.getElementById('villes')))
+                    {
+                        document.getElementById('villes').remove();
+                        console.log("delete villes");
+                    }
+                    if(!document.getElementById('villeDIV').contains(document.getElementById('ville')))
+                    {
+                        var element2 = document.createElement('input');
+                        element2.id = "ville";
+                        document.getElementById('villeDIV').appendChild(element2);
+                    }
+                    document.getElementById('ville').value = json[0].nomVille;
+                }
+
+                document.getElementById('dep').value = json[0].nomCommune;
+            }
         }
-    }
+    };
+    xhr.open("GET", "ajax.php?cp=" + codePostal, true);
+    xhr.send(null);
 }
 
 function getXhr()
@@ -34,4 +98,10 @@ function getXhr()
         xhr = false ;
     }
     return xhr;
+}
+
+function is_numeric(chaine)
+{
+    var exp=new RegExp("^[0-9]*$","g");
+    return exp.test(chaine);
 }
